8581978dcd05655e056df55cfd8427bc588deb9e
Patrick Grady <patickrgrady81@gmail.com>
2020-05-11 18:53:45 -0400

    Backend ready for front end to send data

--- app/controllers/api/v1/compute_controller.rb
+++ app/controllers/api/v1/compute_controller.rb
@@ -1,5 +1,14 @@
+if Rails.env.development?
+  require 'pry'
+end
+
 class API::V1::ComputeController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render json: {hello: true}
+  end
+
+  def sent
+    render json: {"info": params[:compute]}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ root to: "pages#home"
   namespace :api, constraints: {format: :json}, defaults: {format: :json} do
     namespace :v1 do
       resources :compute, only: [:index]
+
+      post "/send", to: "compute#sent"
     end
   end
 end
