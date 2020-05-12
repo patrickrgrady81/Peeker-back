fcb70f8fb475d97c060e12fe999192389421025e
Patrick Grady <patickrgrady81@gmail.com>
2020-05-12 16:37:21 -0400

    computing hand value: hand is now sorted a lowest k highest

--- app/controllers/api/v1/compute_controller.rb
+++ app/controllers/api/v1/compute_controller.rb
@@ -11,16 +11,18 @@ class API::V1::ComputeController < ApplicationController
 
   def sent
     # params[:compute] is everthing sent here
+    hand = params[:compute][:hand]
     case params[:compute][:gameState]
     when "DEAL" 
-      # hand value is nothing
+      # calculate hand value
+      value = getHandValue(hand)
       # best plays is nothing
       # calculate payouts 
       # update credits
-      render json: {status: "IN DEAL"}
+      render json: {status: "IN DEAL", handValue: value}
     when "DRAW"
       # calculate hand value
-      value = getHandValue
+      value = getHandValue(hand)
       # calculate odds
       # calculate best plays
       render json: {status: "IN DRAW", handValue: value}
@@ -29,8 +31,17 @@ class API::V1::ComputeController < ApplicationController
 
   private 
 
-  def getHandValue
+  def getHandValue(hand)
+    #get the ids
+    # ids = []
     handValue = "Nothing"
+
+    sortedHand = hand.sort_by { |card| card["i"] }
+    
+    # Check for straight
+    # Check for flush
+
+
     return handValue
   end
 end
