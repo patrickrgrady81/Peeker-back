57666d5fadfa45dc7733687988d74204a3bd662b
Patrick Grady <patickrgrady81@gmail.com>
2020-05-14 11:31:34 -0400

    Two Pair

--- app/controllers/api/v1/compute_controller.rb
+++ app/controllers/api/v1/compute_controller.rb
@@ -42,6 +42,31 @@ class API::V1::ComputeController < ApplicationController
     # Check for flush
 
 
+    # Two Pair
+    # //  2pairs
+    # //  ------------------
+    # //  1,2 and 3,4
+    # //  1,2 and 4,5
+    # //  2,3 amd 4,5
+    # //  so 2 card will always be there
+    # //  and 4 card will always be there
+    # //  2 is one pair, 4 is the other
+    twoPair = false
+
+    twoPair = true if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"] 
+    twoPair = true if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"] 
+    twoPair = true if sortedHand[1]["v"] == sortedHand[2]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"] 
+    if twoPair
+      if sortedHand[1]["v"] == "A" 
+        return "Two Pair As over #{sortedHand[3]["v"]}s"
+      elsif sortedHand[3]["v"] == "A"
+        return "Two Pair As over #{sortedHand[1]["v"]}s"
+      else
+        return "Two Pair #{sortedHand[3]["v"]}s over #{sortedHand[1]["v"]}s" 
+      end
+    end
+
+
     # One Pair
     for i in 0..3 do 
       if sortedHand[i]["v"] == sortedHand[i+1]["v"]
