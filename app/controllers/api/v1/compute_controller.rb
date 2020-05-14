72921cd682bc39076b3ffbf65d5cc0257cab5dd8
Patrick Grady <patickrgrady81@gmail.com>
2020-05-14 12:50:51 -0400

    Hand Value calcs all done

--- app/controllers/api/v1/compute_controller.rb
+++ app/controllers/api/v1/compute_controller.rb
@@ -32,25 +32,82 @@ class API::V1::ComputeController < ApplicationController
   private 
 
   def getHandValue(hand)
-    #get the ids
-    # ids = []
     handValue = "Nothing"
-
     sortedHand = hand.sort_by { |card| card["i"] }
     
     # Check for straight
+    straight = false;
+    high = nil
+    if sortedHand[0]["i"] == 1 
+      if sortedHand[1]["i"] == 2 && sortedHand[2]["i"] == 3 && sortedHand[3]["i"] == 4 && sortedHand[4]["i"] == 5
+        straight = true
+        high = "5"
+      end
+      if sortedHand[4]["i"] == 13 && sortedHand[3]["i"] == 12 && sortedHand[2]["i"] == 11 && sortedHand[1]["i"] == 10
+        straight = true
+        high = "A"
+      end
+    end
+    if sortedHand[1]["i"] == sortedHand[0]["i"] + 1 && sortedHand[2]["i"] == sortedHand[1]["i"] + 1 &&
+       sortedHand[3]["i"] == sortedHand[2]["i"] + 1 && sortedHand[4]["i"] == sortedHand[3]["i"] + 1 &&
+      straight = true 
+      high = sortedHand[4]["v"]
+    end
+
+
     # Check for flush
+    flush = false
+
+    if sortedHand[0]["s"] == sortedHand[1]["s"] && sortedHand[1]["s"] == sortedHand[2]["s"] && 
+       sortedHand[2]["s"] == sortedHand[3]["s"] && sortedHand[3]["s"] == sortedHand[4]["s"]
+       flush = true
+    end 
+
+    # Royal Flush
+    if straight && flush && high == "A"
+      return "Royal Flush"
+    end
+
+    # Straight Flush 
+    if straight && flush 
+      return "Straight Flush #{high} High"
+    end
+
+    # 4 of a kind
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] ||
+       sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"] 
+       return "Four of a Kind #{sortedHand[3]["v"]}s"
+    end
 
 
-    # Two Pair
-    # //  2pairs
-    # //  ------------------
-    # //  1,2 and 3,4
-    # //  1,2 and 4,5
-    # //  2,3 amd 4,5
-    # //  so 2 card will always be there
-    # //  and 4 card will always be there
-    # //  2 is one pair, 4 is the other
+    #Full House
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
+      return "Full House #{sortedHand[3]["v"]}s Full of #{sortedHand[0]["v"]}s"
+    end
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
+      return "Full House #{sortedHand[0]["v"]}s Full of #{sortedHand[3]["v"]}s"
+    end
+    
+
+    # Flush 
+    if flush
+      return "Flush A High" if sortedHand[0]["i"] == 1 
+      return "Flush #{sortedHand[4]["v"]} High"
+    end
+
+
+    # Straight
+    if straight 
+      return "Straight #{high} High"
+    end
+
+    # Three of a Kind
+    return "Three of a Kind #{sortedHand[0]["v"]}s" if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[1]["v"] == sortedHand[2]["v"]
+    return "Three of a Kind #{sortedHand[1]["v"]}s" if sortedHand[1]["v"] == sortedHand[2]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"]
+    return "Three of a Kind #{sortedHand[2]["v"]}s" if sortedHand[2]["v"] == sortedHand[3]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"]
+
+
+    # Two Pair 
     twoPair = false
 
     twoPair = true if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"] 
