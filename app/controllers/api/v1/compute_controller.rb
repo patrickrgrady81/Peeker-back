811b00b52ae70035141448eea5512c697ee8dd85
Patrick Grady <patickrgrady81@gmail.com>
2020-05-15 10:38:27 -0400

    hand value ranked

--- app/controllers/api/v1/compute_controller.rb
+++ app/controllers/api/v1/compute_controller.rb
@@ -15,18 +15,29 @@ class API::V1::ComputeController < ApplicationController
     case params[:compute][:gameState]
     when "DEAL" 
       # calculate hand value
-      value = getHandValue(hand)
+      val = getHandValue(hand)
+      value = val[0]
+      intValue = val[1]
+
       # best plays is nothing
       # calculate payouts 
       payout = getPayouts
       # update credits
-      render json: {status: "IN DEAL", handValue: value}
+      render json: {status: "IN DRAW", handValue: value, intHandValue: intValue}
     when "DRAW"
       # calculate hand value
-      value = getHandValue(hand)
+      
+      val = getHandValue(hand)
+      value = val[0]
+      intValue = val[1]
+
+      
+      # binding.pry
+      
+      
       # calculate odds
       # calculate best plays
-      render json: {status: "IN DRAW", handValue: value}
+      render json: {status: "IN DRAW", handValue: value, intHandValue: intValue}
     end
   end
 
@@ -34,6 +45,7 @@ class API::V1::ComputeController < ApplicationController
 
   def getHandValue(hand)
     handValue = "Nothing"
+    intHandValue = 0
     sortedHand = hand.sort_by { |card| card["i"] }
     
     # Check for straight
@@ -63,80 +75,95 @@ class API::V1::ComputeController < ApplicationController
        sortedHand[2]["s"] == sortedHand[3]["s"] && sortedHand[3]["s"] == sortedHand[4]["s"]
        flush = true
     end 
-
-    # Royal Flush
-    if straight && flush && high == "A"
-      return "Royal Flush"
-    end
-
-    # Straight Flush 
-    if straight && flush 
-      return "Straight Flush #{high} High"
-    end
-
-    # 4 of a kind
-    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] ||
-       sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"] 
-       return "Four of a Kind #{sortedHand[3]["v"]}s"
-    end
-
-
-    #Full House
-    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
-      return "Full House #{sortedHand[3]["v"]}s Full of #{sortedHand[0]["v"]}s"
-    end
-    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
-      return "Full House #{sortedHand[0]["v"]}s Full of #{sortedHand[3]["v"]}s"
+    
+    # High Card 
+    handValue = "High Card #{sortedHand[4]["v"]}"
+    handValue = "High Card Ace" if sortedHand[0]["i"] == 1
+    
+    # One Pair
+    for i in 0..3 do 
+      if sortedHand[i]["v"] == sortedHand[i+1]["v"]
+        intHandValue = 1
+        handValue = "Pair of #{sortedHand[i]["v"]}s"
+      end
     end
     
-
-    # Flush 
-    if flush
-      return "Flush A High" if sortedHand[0]["i"] == 1 
-      return "Flush #{sortedHand[4]["v"]} High"
-    end
-
-
-    # Straight
-    if straight 
-      return "Straight #{high} High"
-    end
-
-    # Three of a Kind
-    return "Three of a Kind #{sortedHand[0]["v"]}s" if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[1]["v"] == sortedHand[2]["v"]
-    return "Three of a Kind #{sortedHand[1]["v"]}s" if sortedHand[1]["v"] == sortedHand[2]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"]
-    return "Three of a Kind #{sortedHand[2]["v"]}s" if sortedHand[2]["v"] == sortedHand[3]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"]
-
-
     # Two Pair 
     twoPair = false
-
+    
     twoPair = true if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"] 
     twoPair = true if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"] 
     twoPair = true if sortedHand[1]["v"] == sortedHand[2]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"] 
     if twoPair
+      intHandValue = 2
       if sortedHand[1]["v"] == "A" 
-        return "Two Pair As over #{sortedHand[3]["v"]}s"
+        handValue = "Two Pair As over #{sortedHand[3]["v"]}s"
       elsif sortedHand[3]["v"] == "A"
-        return "Two Pair As over #{sortedHand[1]["v"]}s"
+        handValue = "Two Pair As over #{sortedHand[1]["v"]}s"
       else
-        return "Two Pair #{sortedHand[3]["v"]}s over #{sortedHand[1]["v"]}s" 
+        handValue = "Two Pair #{sortedHand[3]["v"]}s over #{sortedHand[1]["v"]}s" 
       end
     end
-
-
-    # One Pair
-    for i in 0..3 do 
-      if sortedHand[i]["v"] == sortedHand[i+1]["v"]
-        return "Pair of #{sortedHand[i]["v"]}s"
-      end
+    
+    # Three of a Kind
+    if sortedHand[0]["v"] == sortedHand[1]["v"] && sortedHand[1]["v"] == sortedHand[2]["v"]
+      intHandValue = 3
+      handValue = "Three of a Kind #{sortedHand[0]["v"]}s" 
+    end
+    if sortedHand[1]["v"] == sortedHand[2]["v"] && sortedHand[2]["v"] == sortedHand[3]["v"]
+      intHandValue = 3
+      handValue = "Three of a Kind #{sortedHand[1]["v"]}s" 
+    end
+    if sortedHand[2]["v"] == sortedHand[3]["v"] && sortedHand[3]["v"] == sortedHand[4]["v"]
+      intHandValue = 3
+      handValue = "Three of a Kind #{sortedHand[2]["v"]}s" 
+    end
+    
+    # Straight
+    if straight 
+      intHandValue = 4
+      handValue = "Straight #{high} High"
     end
 
+    # Flush 
+    if flush
+      intHandValue = 5
+      handValue = "Flush A High" if sortedHand[0]["i"] == 1 
+      handValue = "Flush #{sortedHand[4]["v"]} High"
+    end  
+    
+    
+    #Full House
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
+      intHandValue = 6
+      handValue = "Full House #{sortedHand[3]["v"]}s Full of #{sortedHand[0]["v"]}s"
+    end
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"]
+      intHandValue = 6
+      handValue = "Full House #{sortedHand[0]["v"]}s Full of #{sortedHand[3]["v"]}s"
+    end
 
-    # High Card 
-    handValue = "High Card #{sortedHand[4]["v"]}"
-    handValue = "High Card Ace" if sortedHand[0]["i"] == 1
-    return handValue
+    # 4 of a kind
+    if sortedHand[0]["i"] == sortedHand[1]["i"] && sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] ||
+      sortedHand[1]["i"] == sortedHand[2]["i"] && sortedHand[2]["i"] == sortedHand[3]["i"] && sortedHand[3]["i"] == sortedHand[4]["i"] 
+      
+      intHandValue = 7
+      handValue = "Four of a Kind #{sortedHand[3]["v"]}s"
+   end    
+
+    # Straight Flush 
+    if straight && flush 
+      intHandValue = 8
+      handValue = "Straight Flush #{high} High"
+    end
+
+    # Royal Flush
+    if straight && flush && high == "A"
+      intHandValue = 9
+      handValue = "Royal Flush"
+    end    
+
+    return [handValue, intHandValue]
   end
 
   def getPayouts
